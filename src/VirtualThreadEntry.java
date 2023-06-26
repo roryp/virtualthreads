18f485f43600f5537b70183a81d048aa472f532f
Rory Preddy <rorypreddy@gmail.com>
2023-06-26 08:39:14 +0200

    virtual thread demo

---
+++ src/VirtualThreadEntry.java
@@ -0,0 +1,17 @@
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class VirtualThreadEntry {
+    public static void main(String[] args) throws InterruptedException {
+
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) { // create a new virtual thread executor
+            IntStream.range(0, 10_000) // create a stream of integers from 0 to 9999
+                    .forEach(i -> executor.submit(() -> { // submit a virtual thread for each integer
+                        System.out.println("Hello from virtual thread " + i);
+                        Thread.sleep(Duration.ofSeconds(1)); // sleep for 1 second
+                        return i;
+                    }));
+        }
+    }
+}
