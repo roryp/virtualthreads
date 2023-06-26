f5fa4750acd89452ec3a4a3764e9d9d0a5be7090
Rory Preddy <rorypreddy@gmail.com>
2023-06-26 15:37:16 +0200

    algo fix

--- src/TravelingSalesman.java
+++ src/TravelingSalesman.java
@@ -1,58 +1,80 @@
 import java.util.*;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class TravelingSalesman {
 
     public static void main(String[] args) throws InterruptedException {
         Map<String, Map<String, Integer>> cities = Map.of(
-            "San Francisco", Map.of("Los Angeles", 347, "Denver", 950, "New York", 2572, "Johannesburg", 16983, "Paris", 5583),
-            "Los Angeles", Map.of("San Francisco", 347, "Denver", 830, "New York", 2445, "Johannesburg", 16713, "Paris", 5669),
-            "Denver", Map.of("San Francisco", 950, "Los Angeles", 830, "New York", 1631, "Johannesburg", 15388, "Paris", 4880),
-            "New York", Map.of("San Francisco", 2572, "Los Angeles", 2445, "Denver", 1631, "Johannesburg", 7969, "Paris", 3635),
-            "Johannesburg", Map.of("San Francisco", 16983, "Los Angeles", 16713, "Denver", 15388, "New York", 7969, "Paris", 5410),
-            "Paris", Map.of("San Francisco", 5583, "Los Angeles", 5669, "Denver", 4880, "New York", 3635, "Johannesburg", 5410)
+                "San Francisco", Map.of("Los Angeles", 347, "Denver", 950, "New York", 2572, "Johannesburg", 16983, "Paris", 5583),
+                "Los Angeles", Map.of("San Francisco", 347, "Denver", 830, "New York", 2445, "Johannesburg", 16713, "Paris", 5669),
+                "Denver", Map.of("San Francisco", 950, "Los Angeles", 830, "New York", 1631, "Johannesburg", 15388, "Paris", 4880),
+                "New York", Map.of("San Francisco", 2572, "Los Angeles", 2445, "Denver", 1631, "Johannesburg", 7969, "Paris", 3635),
+                "Johannesburg", Map.of("San Francisco", 16983, "Los Angeles", 16713, "Denver", 15388, "New York", 7969, "Paris", 5410),
+                "Paris", Map.of("San Francisco", 5583, "Los Angeles", 5669, "Denver", 4880, "New York", 3635, "Johannesburg", 5410)
         );
+
         List<String> allCities = new ArrayList<>(cities.keySet());
-        Map<String, Integer> totalDistances = new HashMap<>();
+        Map<String, Integer> totalDistances = new ConcurrentHashMap<>();
+        Map<String, List<String>> routes = new ConcurrentHashMap<>();
         Random random = new Random();
 
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            for (String startCity : allCities) {
-                executor.submit(() -> {
-                    List<String> otherCities = new ArrayList<>(allCities);
-                    otherCities.remove(startCity);
-                    int totalDistance = calculateDistance(startCity, otherCities, cities, random);
-                    System.out.printf("Starting at %s: %d miles\n", startCity, totalDistance);
-                    totalDistances.put(startCity, totalDistance);
-                });
-            }
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+        for (String startCity : allCities) {
+            executor.submit(() -> {
+                List<String> otherCities = new ArrayList<>(allCities);
+                otherCities.remove(startCity);
+                List<String> route = calculateDistance(startCity, otherCities, cities, random);
+                int totalDistance = calculateTotalDistance(route, cities);
+                totalDistances.put(startCity, totalDistance);
+                routes.put(startCity, route);
+            });
         }
 
-        // Let's wait for all tasks to finish before finding the best city
-        Thread.sleep(1000);
+        executor.shutdown();
+        executor.awaitTermination(3, TimeUnit.SECONDS);
 
+        // Print all routes
+        for (String city : allCities) {
+            System.out.printf("Starting at %s, total distance: %s, route: %s\n",
+                    city, formatDistance(totalDistances.get(city)), routes.get(city));
+        }
+
+        // Find the best city to start
         Map.Entry<String, Integer> bestCity = totalDistances.entrySet().stream()
                 .min(Map.Entry.comparingByValue())
                 .orElseThrow();
 
-        System.out.printf("Best city to start is %s with a total distance of %d miles\n", bestCity.getKey(),
-                bestCity.getValue());
+        System.out.printf("\nBest city to start is %s with a total distance of %s miles\n",
+                bestCity.getKey(), formatDistance(bestCity.getValue()));
     }
 
-    private static int calculateDistance(String startCity, List<String> otherCities,
+    private static List<String> calculateDistance(String startCity, List<String> otherCities,
             Map<String, Map<String, Integer>> cities, Random random) {
-        int totalDistance = 0;
+        List<String> route = new ArrayList<>();
         String currentCity = startCity;
 
         while (!otherCities.isEmpty()) {
             String nextCity = findRandomCity(otherCities, random);
-            totalDistance += cities.get(currentCity).get(nextCity);
+            route.add(currentCity);
             currentCity = nextCity;
             otherCities.remove(currentCity);
         }
 
-        // return to start city
-        totalDistance += cities.get(currentCity).get(startCity);
+        // Return to the start city
+        route.add(currentCity);
+        route.add(startCity); // Add the start city to complete the route
+        return route;
+    }
+
+    private static int calculateTotalDistance(List<String> route, Map<String, Map<String, Integer>> cities) {
+        int totalDistance = 0;
+
+        for (int i = 0; i < route.size() - 1; i++) {
+            String currentCity = route.get(i);
+            String nextCity = route.get(i + 1);
+            totalDistance += cities.get(currentCity).get(nextCity);
+        }
+
         return totalDistance;
     }
 
@@ -60,4 +82,10 @@ public class TravelingSalesman {
         int randomIndex = random.nextInt(cities.size());
         return cities.get(randomIndex);
     }
+
+    private static String formatDistance(Integer distance) {
+        return distance != null ? distance.toString() : "null";
+    }
+
 }
+   
